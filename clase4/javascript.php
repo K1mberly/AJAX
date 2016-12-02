310bdb0e7a5c6e947a0e564e87f84686b228ed51
K1mberly <kim.ruiz.g@gmail.com>
2016-12-02 13:01:17 -0500

    inicializar ajax

---
+++ clase4/javascript.php
@@ -0,0 +1,16 @@
+Ready States
+
+
+0: Request not initialized
+1: Server connection stablished
+2: Request received
+3: Processing request
+4: Request finished and response is ready
+
+
+Status
+
+200 is Ok
+
+
+404 the page was not found
